7da55ee673cca1be61cd724b19d2b611f252c5a9
AmaseCocoa <amase.cocoa@gmail.com>
2024-11-29 14:46:58 +0900

    feat(backend): load config from /.config/config.yml

---
+++ packages/backend/scripts/load_conf.py
@@ -0,0 +1,7 @@
+from omegaconf import OmegaConf
+import os
+
+def load():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(script_dir, '..', '..', '..', '.config', 'config.yml')
+    config = OmegaConf.load(config_path)
