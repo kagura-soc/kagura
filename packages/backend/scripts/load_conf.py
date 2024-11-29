2ceb1534a117997721f05fe03acbcb81921d0cdb
AmaseCocoa <amase.cocoa@gmail.com>
2024-11-29 18:07:39 +0900

    fix(backend): fixed missing return value

--- packages/backend/scripts/load_conf.py
+++ packages/backend/scripts/load_conf.py
@@ -5,3 +5,4 @@ def load():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     config_path = os.path.join(script_dir, '..', '..', '..', '.config', 'config.yml')
     config = OmegaConf.load(config_path)
+    return config
