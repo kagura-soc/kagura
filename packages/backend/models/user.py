79cace74bb3a5ef4e3e3a7e897bed7d70a14c226
AmaseCocoa <amase.cocoa@gmail.com>
2024-11-29 18:10:20 +0900

    feat(backend): totp, federation support

--- packages/backend/models/user.py
+++ packages/backend/models/user.py
@@ -1,13 +1,15 @@
-import re
-
 from piccolo.table import Table
-from piccolo.columns import ForeignKey, Varchar, Text, UUID
+from piccolo.columns import ForeignKey, Varchar, Text, UUID, Boolean
 
 class KFollows(Table):
     id = UUID()
     followee = ForeignKey("KUser", null=False)
     follower = ForeignKey("KUser", null=False)
 
+class KIPLog(Table):
+    id = UUID()
+    user = ForeignKey("KUser", null=False)
+    ip_address = Text()
 
 class KUser(Table):
     id = Text()
@@ -16,9 +18,18 @@ class KUser(Table):
     
     email = Text(unique=True, null=True) # TODO: バックエンド側で^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$に合わない場合は弾く
     password = Text()
+    totp_seed = Text()
     
     followers = ForeignKey("KFollows", related_name="followee")
     following = ForeignKey("KFollows", related_name="follower")
     
     notes = ForeignKey("KNote", related_name="user")
     reactions = ForeignKey("KReaction", related_name="user")
+    
+    public_key = Text() 
+    private_key = Text(default=None)
+    
+    host = ForeignKey("KHost", default=None)
+    
+    is_suspended = Boolean()
+    is_muted = Boolean()
